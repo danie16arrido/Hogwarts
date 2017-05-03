55c5c6e2559d66b450b1926f219e7e1d7f693c55
Lewis <lewisburns630@googlemail.com>
2017-05-03 15:31:52 +0100

    test all students view

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -2,8 +2,14 @@ require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('./models/student.rb')
+require_relative('./db/sqlrunner.rb')
 
 get '/home' do
+  erb(:home)
+end
+
+get '/students' do 
+  @all_students = Student.all()
   erb(:index)
 end
 
